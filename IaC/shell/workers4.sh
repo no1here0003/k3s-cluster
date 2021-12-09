e6bb23d81155fac9975c9b0ffaa376e43c614349
no1here0003 <masternater678@gmail.com>
2021-12-09 15:01:11 -0500

    Cleanup

--- IaC/shell/workers4.sh
+++ IaC/shell/workers4.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-
-k3sup join \
-  --ip 192.168.2.202 \
-  --user kube \
-  --k3s-channel stable \
-  --server-ip 192.168.2.100 \
-  --server-user kube
-
 k3sup join \
   --ip 192.168.2.122 \
   --user kube \
