16580e28a78f6a0794fada3cb754d52fc8fc0015
no1here0003 <masternater678@gmail.com>
2021-12-12 08:37:05 -0500

    Ready

--- hack/fluxcd.sh
+++ hack/fluxcd.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+
+kubectl create ns flux-system
+
+kubectl apply -f ~/secrets/cluster-secrets.yaml
+
+cat ~/.config/sops/age/keys.txt | kubectl -n flux-system create secret generic sops-age \
+--from-file=age.agekey=/dev/stdin
+
+
+
 flux bootstrap github \
   --owner=no1here0003 \
   --repository=home-cluster \
   --branch=master \
   --path=./apps/base \
   --personal 
+
+
