737d1a067848d4b6fb355d6b036ea24de371d613
no1here0003 <masternater678@gmail.com>
2021-12-16 18:23:23 -0500

    Slowly

--- IaC/shell/fan_50.sh
+++ IaC/shell/fan_50.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/sh -vxf
 
 # Used to reduce fan speed on R720 to 50%
+# Enable control
+ipmitool -I lanplus -H $IDRAC_IP -U $IDRAC_USERNAME -P $IDRAC_PASSWORD raw 0x30 0x30 0x01 0x00
 
-ipmitool -I lanplus -H <ip here> -U root -P 'password here' raw 0x30 0x30 0x02 0xff 0x32
+# Set to 50%
+ipmitool -I lanplus -H $IDRAC_IP -U $IDRAC_USERNAME -P $IDRAC_PASSWORD raw 0x30 0x30 0x02 0xff 0x32
