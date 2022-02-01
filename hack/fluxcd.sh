db89404ec4454472884b642637ed8cc3e48eed0e
no1here0003 <no1here0003>
2022-02-01 07:27:43 -0500

    Correct settings

--- hack/fluxcd.sh
+++ hack/fluxcd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 kubectl create ns flux-system
-
+sleep 5
 kubectl apply -f ~/secrets/cluster-secrets.yaml
-
+kubectl apply -f ~/secrets/cluster-settings.yaml
 cat ~/.config/sops/age/keys.txt | kubectl -n flux-system create secret generic sops-age \
 --from-file=age.agekey=/dev/stdin
 
