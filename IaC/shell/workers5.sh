f55a970de9d0ed856519f77df8aa065857f2e328
no1here0003 <no1here0003>
2022-02-05 20:48:25 -0500

    Fix variable name

--- IaC/shell/workers5.sh
+++ IaC/shell/workers5.sh
@@ -2,57 +2,8 @@
 
 
 k3sup join \
-  --ip 192.168.2.131 \
+  --ip 192.168.2.254 \
   --user kube \
   --k3s-channel latest \
-  --server-ip 192.168.2.100 \
-  --server-user kube
-
-k3sup join \
-  --ip 192.168.2.132 \
-  --user kube \
-  --k3s-channel latest \
-  --server-ip 192.168.2.100 \
-  --server-user kube
-
-k3sup join \
-  --ip 192.168.2.133 \
-  --user kube \
-  --k3s-channel latest \
-  --server-ip 192.168.2.100 \
-  --server-user kube
-
-k3sup join \
-  --ip 192.168.1.10 \
-  --user kube \
-  --k3s-channel latest \
-  --server-ip 192.168.2.100 \
-  --server-user kube
-
-k3sup join \
-  --ip 192.168.2.140 \
-  --user kube \
-  --k3s-channel latest \
-  --server-ip 192.168.2.100 \
-  --server-user kube
-
-k3sup join \
-  --ip 192.168.2.141 \
-  --user kube \
-  --k3s-channel latest \
-  --server-ip 192.168.2.100 \
-  --server-user kube
-
-k3sup join \
-  --ip 192.168.2.142 \
-  --user kube \
-  --k3s-channel latest \
-  --server-ip 192.168.2.100 \
-  --server-user kube
-
-k3sup join \
-  --ip 192.168.2.143 \
-  --user kube \
-  --k3s-channel latest \
-  --server-ip 192.168.2.100 \
+  --server-ip 192.168.2.5 \
   --server-user kube
