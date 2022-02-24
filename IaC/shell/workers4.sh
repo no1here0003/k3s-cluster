202c504c92dc5767f8385a6475a8f7f747213161
no1here0003 <no1here0003>
2022-02-24 17:12:22 -0500

    Harbor start

--- IaC/shell/workers4.sh
+++ IaC/shell/workers4.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
-
 k3sup join \
-  --ip 192.168.3.29 \
+  --ip 192.168.3.31 \
   --user kube \
   --k3s-channel latest \
   --server-ip 192.168.3.5 \
   --server-user kube
 
 k3sup join \
-  --ip 192.168.3.30 \
+  --ip 192.168.3.32 \
+  --user kube \
+  --k3s-channel latest \
+  --server-ip 192.168.3.5 \
+  --server-user kube
+
+k3sup join \
+  --ip 192.168.3.33 \
+  --user kube \
+  --k3s-channel latest \
+  --server-ip 192.168.3.5 \
+  --server-user kube
+
+k3sup join \
+  --ip 192.168.3.34 \
   --user kube \
   --k3s-channel latest \
   --server-ip 192.168.3.5 \
